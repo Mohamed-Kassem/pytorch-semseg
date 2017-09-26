d89a912d0d2cbf9af79f326d23aae35949f9d78f
Mohamed Kassem <mohamed.samy.k@gmail.com>
2017-09-26 21:59:46 +0200

    adding resume capability

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ from ptsemseg.metrics import scores
 from lr_scheduling import *
 
 import time
-import sys
+import os
 
 def train(args):
     # time start
@@ -53,8 +53,22 @@ def train(args):
 
     optimizer = torch.optim.SGD(model.parameters(), lr=args.l_rate, momentum=0.99, weight_decay=5e-4)
 
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            #best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+
     loss_arr = [-1]*len(trainloader)*(args.n_epoch+1)
-    for epoch in range(args.n_epoch):
+    print("Train data size: ", len(trainloader)*args.batch_size)
+    for epoch in range(args.start_epoch, args.n_epoch):
         for i, (images, labels) in enumerate(trainloader):
             print('iteration: {}'.format(i))
             if torch.cuda.is_available():
@@ -82,7 +96,7 @@ def train(args):
             #     update='append')
 
             loss_arr[i] = loss.data[0]
-            if (i+1) % 20 == 0:
+            if (i+1) % 1 == 0:
                 #print("Epoch [%d/%d] Loss: %.4f" % (epoch+1, args.n_epoch, loss.data[0]))
                 end = time.time()
                 print("Epoch [%d/%d] Iteration [%d/%d] Loss: %.4f time(sec): %.1f" % (epoch+1, args.n_epoch, i, len(trainloader), loss.data[0], end-start))
@@ -95,10 +109,23 @@ def train(args):
         # vis.image(test_image[0].cpu().data.numpy(), opts=dict(title='Input' + str(epoch)))
         # vis.image(np.transpose(target, [2,0,1]), opts=dict(title='GT' + str(epoch)))
         # vis.image(np.transpose(predicted, [2,0,1]), opts=dict(title='Predicted' + str(epoch)))
-        np.save("loss_array_epoch_{}.npy".format(epoch), loss_arr)
+        np.save("loss_array_epoch_{}.npy".format(epoch+1), loss_arr)
         # GCP storage!
-        if (epoch+1)%2 == 0:
-            torch.save(model, "{}_{}_{}_{}.pkl".format(args.arch, args.dataset, args.feature_scale, epoch))
+        #if (epoch+1)%2 == 0:
+            #torch.save(model, "{}_{}_{}_{}.pkl".format(args.arch, args.dataset, args.feature_scale, epoch))
+        save_checkpoint({
+                    'epoch': epoch + 1,
+                    'arch': args.arch,
+                    'state_dict': model.state_dict(),
+                    #'best_prec1': best_prec1,
+                    'optimizer' : optimizer.state_dict(),
+                }, False, epoch, 2)
+
+def save_checkpoint(state, is_best, epoch, max_to_keep=5, filename='checkpoint.pth.tar'):
+    filename = filename.split('.')[0] + '_' + str(epoch%max_to_keep) + '.' + '.'.join(filename.split('.')[1:])
+    torch.save(state, filename)
+    #if is_best:
+    #    shutil.copyfile(filename, 'model_best.pth.tar')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Hyperparams')
@@ -117,7 +144,12 @@ if __name__ == '__main__':
     parser.add_argument('--l_rate', nargs='?', type=float, default=1e-5, 
                         help='Learning Rate')
     parser.add_argument('--feature_scale', nargs='?', type=int, default=1, 
-                        help='Divider for # of features to use')    
+                        help='Divider for # of features to use')
+    parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                        help='path to latest checkpoint (default: none)')
+    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+
     args = parser.parse_args()
     #f = open("logs", 'w')
     #sys.stdout = f
