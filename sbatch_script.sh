1b83fb13bd4ee797537ff077e2d5d57ee64af1d6
Mohamed Kassem <mohamed.samy.k@gmail.com>
2017-10-02 21:34:42 +0200

    some bashy lines

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -19,10 +19,10 @@ if [ "$1" == "validate" ]
 then
     #VALIDATION
     echo "$1"
-    FCN_EPOCH=1
+    FCN_EPOCH="$2"
     export CUDA_VISIBLE_DEVICES=0
     python validate.py --arch fcn8s --model_path "val_fcn8s_epoch{$FCN_EPOCH}.pth.tar" --dataset pascal --img_rows 256 --img_cols 256 --batch_size 1 --split val --cuda_index 0 > fcn8s-epoch{$FCN_EPOCH}.out &
-    SEG_EPOCH=1
+    SEG_EPOCH="$3"
     export CUDA_VISIBLE_DEVICES=1
     python validate.py --arch segnet --model_path val_segnet_epoch{$SEG_EPOCH}.pth.tar --dataset pascal --img_rows 256 --img_cols 256 --batch_size 1 --split val --cuda_index 0 > segnet-epoch{$SEG_EPOCH}.out &
 elif [ "$1" == "train" ]
