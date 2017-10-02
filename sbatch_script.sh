8d443161623928d6ba43e0d9b36c48fed2ddacb6
Mohamed Kassem <M.Kasem@nu.edu.eg>
2017-10-02 09:50:02 +0200

    no multiple gpus charm YET

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -8,4 +8,4 @@
 #SBATCH --time=96:00:00
 
 #python just_import.py
-python train.py --arch fcn8s --dataset pascal --n_epoch 150 --img_rows 256 --img_cols 256 --batch_size 16
+python train.py --arch segnet --dataset pascal --n_epoch 150 --img_rows 256 --img_cols 256 --batch_size 16
