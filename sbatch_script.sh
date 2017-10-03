1a0e63617eb7c62d1a74d09bb91883bf44750bf8
Mohamed Kassem <mohamed.samy.k@gmail.com>
2017-10-03 14:50:07 +0200

    learning rate change

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -31,10 +31,11 @@ then
     echo "********** $1 **********"
     BATCH_SIZE=1
     export CUDA_VISIBLE_DEVICES=0
-    python train.py --arch fcn8s --dataset pascal --n_epoch 150 --img_rows 256 --img_cols 256 --batch_size ${BATCH_SIZE} --cuda_index 0 > "${SLURM_JOB_ID}_batch_size_${BATCH_SIZE}_fcn8s.out" &
+    # python train.py --arch fcn8s --dataset pascal --n_epoch 150 --img_rows 256 --img_cols 256 --batch_size ${BATCH_SIZE} --cuda_index 0 > "${SLURM_JOB_ID}_batch_size_${BATCH_SIZE}_fcn8s.out" &
+    python train.py --arch segnet --dataset pascal --n_epoch 150 --img_rows 256 --img_cols 256 --batch_size ${BATCH_SIZE} --cuda_index 0 --l_rate 1e-2 > "${SLURM_JOB_ID}_lr1e-2_batch_size_${BATCH_SIZE}_segnet.out" &
 
     export CUDA_VISIBLE_DEVICES=1
-    python train.py --arch segnet --dataset pascal --n_epoch 150 --img_rows 256 --img_cols 256 --batch_size ${BATCH_SIZE} --cuda_index 0 --l_rate 1e-2 > "${SLURM_JOB_ID}_batch_size_${BATCH_SIZE}_segnet.out" &
+    python train.py --arch segnet --dataset pascal --n_epoch 150 --img_rows 256 --img_cols 256 --batch_size ${BATCH_SIZE} --cuda_index 0 --l_rate 1e-1 > "${SLURM_JOB_ID}_lr1e-1_batch_size_${BATCH_SIZE}_segnet.out" &
 else
     echo "$1 is unrecognized input"
 fi
