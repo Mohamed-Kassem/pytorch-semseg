1e8d7f4f076d004c210c473a0f500d4ec55edd4b
MohamedSamy <Mohamed.Samy.K@gmail.com>
2017-10-04 15:21:37 +0200

    restart

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -31,7 +31,7 @@ then
     echo "********** $1 **********"
     BATCH_SIZE=1
     # export CUDA_VISIBLE_DEVICES=0
-    python train.py --arch fcn8s --dataset pascal --n_epoch 150 --img_rows 256 --img_cols 256 --batch_size 32 --exp_index 0 --validate_every 5 --job_id ${SLURM_JOB_ID} --resume checkpoint_1.pth.tar> "${SLURM_JOB_ID}_batch_size_${BATCH_SIZE}_fcn8s.out" 2>&1 &
+    python train.py --arch fcn8s --dataset pascal --n_epoch 150 --img_rows 256 --img_cols 256 --batch_size 16 --exp_index 0 --validate_every 5 --job_id ${SLURM_JOB_ID} --resume checkpoint_1.pth.tar> "${SLURM_JOB_ID}_batch_size_${BATCH_SIZE}_fcn8s.out" 2>&1 &
     # python train.py --arch segnet --dataset pascal --n_epoch 150 --img_rows 256 --img_cols 256 --batch_size ${BATCH_SIZE} --cuda_index 0 --l_rate 1e-4 > "${SLURM_JOB_ID}_lr1e-4_batch_size_${BATCH_SIZE}_segnet.out" 2>&1 &
 
     # export CUDA_VISIBLE_DEVICES=1
